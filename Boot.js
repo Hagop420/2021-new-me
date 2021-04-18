fcf4486c9a5b18eebeba6d3bc7dbf43a947d94ad
Hagop Almadjian <hbamb420@Hagops-MBP.charter.com>
2021-04-18 01:04:35 -0700

    first commit

---
+++ Boot.js
@@ -0,0 +1,15 @@
+var collapseGone = document.getElementById('sad')
+collapseGone.addEventListener('click', function(){
+    if(collapseGone.contains=== 'Triller 
+    Fight Club is a 
+    new entertainment 
+    platform 
+    which pairs 
+    four quadrant entertainment 
+    with boxing that even 
+    the staunchest purist will love. 
+    This fight will certainly go down as iconic and TFC will make sure it is presented with entertainment for every audience, from the diehard to the casual fan. 
+    These two undefeated stars transcend their sport not just in the U.S. but around the world, and we are working to reshape 
+    the vision of 
+    excitement and storytelling in a sport we love")
+})
